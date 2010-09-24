3a7f147804083f8ef9c542fbaa65d8faf3a4e43d
franck.rasolo <franck.rasolo@e10659e9-a2d5-cffb-be99-d89750fd1a71>
2010-09-24 13:06:38 +0000

    Introduced abstractions that encapsulate the construction of cluster properties using a fluent interface Reinstated autostart=true in simpleServerConfig.xml to make all tests in ClusterStarterTest pass again Reduced the number of servers in cluster.properties to give the CI build a fighting chance of passing again

---
+++ trunk/sandbox/src/main/java/org/gridman/classloader/ClusterNodeProperties.java
@@ -0,0 +1,151 @@
+package org.gridman.classloader;
+
+import java.util.Properties;
+
+public class ClusterNodeProperties {
+
+	private final int numberOfServersToStart;
+	private final String fullyQualifiedClassName;
+	private final Properties properties = new Properties();
+
+	public ClusterNodeProperties(int numberOfServersToStart, Class<? extends ClassloaderLifecycle> clazz) {
+		this.fullyQualifiedClassName = clazz.getName();
+		this.numberOfServersToStart = numberOfServersToStart;
+
+		useDefaultClusterProperties();
+		useDefaultMulticastProperties();
+		useDefaultJmxProperties();
+	}
+
+	public int getNumberOfServersToStart() {
+		return numberOfServersToStart;
+	}
+
+	public String getFullyQualifiedClassName() {
+		return fullyQualifiedClassName;
+	}
+
+	private void useDefaultClusterProperties() {
+		withPofEnabled(true);
+		withCoherenceOverride("coherence/simple/simpleOverride.xml");
+		withClusterIdentifier("anew-1");
+		withLogLevel(9);
+		withCoherenceExtendPort(8098);
+
+		// we currently turn of the guard because otherwise it kills things
+		properties.setProperty("tangosol.coherence.guard.timeout", "0");
+	}
+
+	private void useDefaultJmxProperties() {
+		withRemoteManagement(true);
+		withRemoteJmxOverSsl(false);
+		withRemoteJmxAuthentication(false);
+	}
+
+	private void useDefaultMulticastProperties() {
+		withClusterPort(8016);
+		withTimeToLive(0);
+	}
+
+	public ClusterNodeProperties withPofEnabled(boolean enabled) {
+		properties.setProperty("tangosol.pof.enabled", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withCoherenceOverride(String xmlConfigFile) {
+		properties.setProperty("tangosol.coherence.override", xmlConfigFile);
+		return this;
+	}
+
+	public ClusterNodeProperties withClusterIdentifier(String identifier) {
+		properties.setProperty("tangosol.coherence.cluster", identifier);
+		return this;
+	}
+
+	public ClusterNodeProperties withLogLevel(int logLevel) {
+//		properties.setProperty("tangosol.coherence.log", "log4j");
+		properties.setProperty("tangosol.coherence.log.level", Integer.toString(logLevel));
+		return this;
+	}
+
+	public ClusterNodeProperties withCoherenceExtendPort(int portNumber) {
+		properties.setProperty("coherence.extend.port", Integer.toString(portNumber));
+		return this;
+	}
+
+	public ClusterNodeProperties withClusterPort(int portNumber) {
+		properties.setProperty("tangosol.coherence.clusterport", Integer.toString(portNumber));
+		return this;
+	}
+
+	public ClusterNodeProperties withTimeToLive(int timeToLive) {
+		properties.setProperty("tangosol.coherence.ttl", Integer.toString(timeToLive));
+		return this;
+	}
+
+	public ClusterNodeProperties withRemoteManagement(boolean enabled) {
+		properties.setProperty("com.sun.management.jmxremote", Boolean.toString(enabled));
+		properties.setProperty("tangosol.coherence.management.remote", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withRemoteJmxOverSsl(boolean enabled) {
+		properties.setProperty("com.sun.management.jmxremote.ssl", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withRemoteJmxAuthentication(boolean enabled) {
+		properties.setProperty("com.sun.management.jmxremote.authenticate", Boolean.toString(enabled));
+		return this;
+	}
+
+	public void useDefaultClientProperties() {
+		withCacheConfig("client.xml");
+		withDistributedLocalStorage(false);
+		withTcmpEnabled(false);
+	}
+
+	public ClusterNodeProperties useDefaultServerProperties() {
+		withDataBackups(1);
+		withCacheConfig("server.xml");
+		withTcmpEnabled(true);
+		withDistributedLocalStorage(true);
+		withStorageEnabled(true);
+		withProxyEnabled(false);
+		return this;
+	}
+
+	public ClusterNodeProperties withDataBackups(int numberOfDataBackups) {
+		properties.setProperty("tangosol.coherence.distributed.backupcount", Integer.toString(numberOfDataBackups));
+		return this;
+	}
+
+	public ClusterNodeProperties withCacheConfig(String xmlConfigFile) {
+		properties.setProperty("tangosol.coherence.cacheconfig", xmlConfigFile);
+		return this;
+	}
+
+	public ClusterNodeProperties withDistributedLocalStorage(boolean enabled) {
+		properties.setProperty("tangosol.coherence.distributed.localstorage", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withStorageEnabled(boolean enabled) {
+		properties.setProperty("coherence.isStorage", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withTcmpEnabled(boolean enabled) {
+		properties.setProperty("tangosol.coherence.tcmp.enabled", Boolean.toString(enabled));
+		return this;
+	}
+
+	public ClusterNodeProperties withProxyEnabled(boolean enabled) {
+		properties.setProperty("coherence.isProxy", Boolean.toString(enabled));
+		return this;
+	}
+
+	public Properties asProperties() {
+		return properties;
+	}
+}
--- trunk/sandbox/src/test/java/org/gridman/classloader/ClusterStarterTest.java
+++ trunk/sandbox/src/test/java/org/gridman/classloader/ClusterStarterTest.java
@@ -2,6 +2,7 @@ package org.gridman.classloader;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.Cluster;
+import org.gridman.classloader.coherence.CoherenceClassloaderLifecycle;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -64,23 +65,33 @@ public class ClusterStarterTest {
         return members.size() - 1;
     }
 
+	@Test
+	public void shouldStartAndStopClusterUsingAPropertiesInstanceBuiltProgrammatically() {
+		ClusterProperties cluster = new ClusterProperties();
+
+		cluster.addNodeWithServers(2, CoherenceClassloaderLifecycle.class)
+				.useDefaultServerProperties()
+				.withCacheConfig("coherence/simple/simpleServerConfig.xml");
+
+		cluster.addNodeWithServers(1, CoherenceClassloaderLifecycle.class)
+				.useDefaultServerProperties()
+				.withCacheConfig("coherence/simple/simpleServerConfig.xml")
+				.withDistributedLocalStorage(false)
+				.withProxyEnabled(true);
+
+		String identifier = "test.cluster";
+		clusterStarter.ensureCluster(identifier, cluster.asProperties());
+		int membersStarted = getClusterSize();
+
+		clusterStarter.shutdown(identifier);
+		int finalSize = getClusterSize();
+
+		assertEquals("Expected to start 3 members", 3, membersStarted);
+		assertEquals("Expected to shutdown all members", 0, finalSize);
+	}
+
     @Test
-    public void shouldStartAndStopClusterUsingAPropertiesInstance() throws Throwable {
-        String clusterFile = "/coherence/classloader/cluster.properties";
-        Properties clusterProperties = SystemPropertyLoader.getSystemProperties(clusterFile);
-
-		clusterStarter.ensureCluster(clusterFile, clusterProperties);
-        int membersStarted = getClusterSize();
-
-        clusterStarter.shutdown(clusterFile);
-        int finalSize = getClusterSize();
-
-        assertEquals("Expected to start 4 members", 4, membersStarted);
-        assertEquals("Expected to shutdown all members", 0, finalSize);
-    }
-
-    @Test
-    public void shouldStartAndStopClusterUsingAClusterFile() throws Throwable {
+    public void shouldStartAndStopClusterUsingAClusterFile() {
         String clusterFile = "/coherence/classloader/cluster.properties";
 
         clusterStarter.ensureCluster(clusterFile);
@@ -89,12 +100,12 @@ public class ClusterStarterTest {
         clusterStarter.shutdown(clusterFile);
         int finalSize = getClusterSize();
 
-        assertEquals("Expected to start 4 members", 4, membersStarted);
+		assertEquals("Expected to start 3 members", 3, membersStarted);
         assertEquals("Expected to shutdown all members", 0, finalSize);
     }
 
     @Test
-    public void shouldStartAndStopClusterGroup() throws Throwable {
+    public void shouldStartAndStopClusterGroup() {
         String clusterFile = "/coherence/classloader/cluster.properties";
         Properties properties = SystemPropertyLoader.getSystemProperties(clusterFile);
 
@@ -104,12 +115,12 @@ public class ClusterStarterTest {
         clusterStarter.shutdown(clusterFile, 0);
         int finalSize = getClusterSize();
 
-        assertEquals("Expected to start 3 members", 3, membersStarted);
+        assertEquals("Expected to start 2 members", 2, membersStarted);
         assertEquals("Expected to shutdown all members", 0, finalSize);
     }
 
     @Test
-    public void shouldStartAndStopSingleInstance() throws Throwable {
+    public void shouldStartAndStopSingleInstance() {
         String clusterFile = "/coherence/classloader/cluster.properties";
         Properties properties = SystemPropertyLoader.getSystemProperties(clusterFile);
 
@@ -122,7 +133,7 @@ public class ClusterStarterTest {
     }
 
     @Test
-    public void shouldStartWholeClusterAndStopGroup() throws Throwable {
+    public void shouldStartWholeClusterAndStopGroup() {
         String clusterFile = "/coherence/classloader/cluster.properties";
 
         clusterStarter.ensureCluster(clusterFile);
@@ -134,13 +145,13 @@ public class ClusterStarterTest {
         clusterStarter.shutdown(clusterFile);
         int finalSize = getClusterSize();
 
-        assertEquals("Expected to start 4 members", 4, membersStarted);
+		assertEquals("Expected to start 3 members", 3, membersStarted);
         assertEquals("Expected to start 1 member after shutdown of group 0", 1, afterGroupShutdownSize);
         assertEquals("Expected to shutdown all members", 0, finalSize);
     }
 
     @Test
-    public void shouldStartWholeClusterAndStopSingleNode() throws Throwable {
+    public void shouldStartWholeClusterAndStopSingleNode() {
         String clusterFile = "/coherence/classloader/cluster.properties";
 
         clusterStarter.ensureCluster(clusterFile);
@@ -152,8 +163,8 @@ public class ClusterStarterTest {
         clusterStarter.shutdown(clusterFile);
         int finalSize = getClusterSize();
 
-        assertEquals("Expected to start 4 members", 4, membersStarted);
-        assertEquals("Expected to start 3 members after shutdown of node", 3, afterNodeShutdownSize);
+		assertEquals("Expected to start 3 members", 3, membersStarted);
+        assertEquals("Expected to start 3 members after shutdown of node", 2, afterNodeShutdownSize);
         assertEquals("Expected to shutdown all members", 0, finalSize);
     }
 }
