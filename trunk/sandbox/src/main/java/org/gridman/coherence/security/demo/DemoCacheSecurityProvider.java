13c66fa8a3a80f6af5323f46e62382982ae44d34
andrew.wilson@orangephoenix.com <andrew.wilson@orangephoenix.com@e10659e9-a2d5-cffb-be99-d89750fd1a71>
2010-09-28 15:19:59 +0000

    next pass at permission code.

--- trunk/sandbox/src/main/java/org/gridman/coherence/security/demo/DemoCacheSecurityProvider.java
+++ trunk/sandbox/src/main/java/org/gridman/coherence/security/demo/DemoCacheSecurityProvider.java
@@ -1,16 +1,10 @@
 package org.gridman.coherence.security.demo;
 
-import com.tangosol.net.CacheFactory;
-import com.tangosol.net.InvocationService;
-import com.tangosol.net.Member;
 import org.apache.log4j.Logger;
-import org.gridman.coherence.security.simple.CoherenceUtils;
 import org.gridman.coherence.security.simple.CacheSecurityProvider;
+import org.gridman.coherence.security.simple.CoherenceUtils;
 
 import javax.security.auth.Subject;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Our Security Provider, it checks the permission in the permissionCache.
@@ -23,15 +17,9 @@ public class DemoCacheSecurityProvider implements CacheSecurityProvider {
     }
 
     /*
-     * We need to use an Invoke Service or we'll get cache loopback.
-     * Otherwise, we could make the PermissionCache read-only to everyone, which I don't like.
+     * check the access.
      */
     @Override public boolean checkAccess(Subject subject, String cacheName, boolean readOnly) {
-        logger.debug("checkAccess " + cacheName + " : " + readOnly + " : " + subject);
-        String role = CoherenceUtils.getFirstPrincipalName(subject);
-        InvocationService service = (InvocationService) CacheFactory.getService(DemoServer.SERVER_INVOKE_SERVICE);
-        Set<Member> memberSet = Collections.singleton(CacheFactory.getCluster().getLocalMember());
-        Map map = service.query(new DemoCachePermissionInvoke(role, cacheName, true, readOnly), memberSet);
-        return (Boolean)map.values().iterator().next();
+        return DemoServer.checkPermission(CoherenceUtils.getFirstPrincipalName(subject),cacheName,true,readOnly);
     }
 }
--- trunk/sandbox/src/main/java/org/gridman/coherence/security/demo/DemoServer.java
+++ trunk/sandbox/src/main/java/org/gridman/coherence/security/demo/DemoServer.java
@@ -3,16 +3,22 @@ package org.gridman.coherence.security.demo;
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.DefaultCacheServer;
 import com.tangosol.net.NamedCache;
+import com.tangosol.util.Filter;
+import com.tangosol.util.filter.AndFilter;
+import com.tangosol.util.filter.EqualsFilter;
+import org.apache.log4j.Logger;
 import org.gridman.classloader.SystemPropertyLoader;
+
+import java.util.Set;
 
 /**
  * A simple Demo Server
  */
 public class DemoServer {
+    private static final Logger logger = Logger.getLogger(DemoServer.class);
+
     static final String PERMISSION_CACHE = "PermissionCache";
     static final String CLIENT_INVOKE_SERVICE = "ClientInvokeService";
-    static final String SERVER_INVOKE_SERVICE = "ServerInvokeService";
-
 
     public static void main(String[] args) throws InterruptedException {
         SystemPropertyLoader.loadSystemProperties("/coherence/security/demo/securityDemoDefault.properties");
@@ -21,5 +27,21 @@ public class DemoServer {
         DemoSecurityPermission perm = new DemoSecurityPermission("admin", PERMISSION_CACHE, true, false);
         cache.put(perm,perm);
         synchronized(DemoServer.class) { DemoServer.class.wait(); }
+    }
+
+    /**
+     * check the permission in the cache, used by both cache + invocation.  This is not the best place for this method but will do.
+     */
+    public static boolean checkPermission(String checkRole, String checkResourceName, boolean isCacheRatherThanInvoke, boolean checkReadOnly) {
+        NamedCache permissionCache = CacheFactory.getCache(DemoServer.PERMISSION_CACHE);
+        Filter filter = new AndFilter(  new EqualsFilter("getRole", checkRole), new EqualsFilter("isCacheRatherThanInvoke",isCacheRatherThanInvoke));
+        Set<DemoSecurityPermission> set = permissionCache.keySet(filter);
+        logger.debug("Got " + set.size());
+        for(DemoSecurityPermission perm : set) {
+            if(perm.matchPermission(checkRole,checkResourceName,isCacheRatherThanInvoke,checkReadOnly)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
